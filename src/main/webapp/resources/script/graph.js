93cfcf475417147a0b30b6bef246b964edc8cc2a
sejun <sejun2.lee@gmail.com>
2024-08-23 17:26:34 +0900

    주석처리

--- src/main/webapp/resources/script/graph.js
+++ src/main/webapp/resources/script/graph.js
@@ -1,12 +1,13 @@
 
 
-function barChart(grapeData) {
+function barChart(grapeData) {	//막대그래프 
 	
 	var ctx = document.getElementById("barCanvas").getContext("2d");
 
 	var data = {
 		labels: ["에너지", "물", "단백질", "지방", "탄수화물", "당", "식이섬유", "칼슘", "철분", "인", "칼륨", "나트륨", "비타민A", "레니놀",
 			"베타카로틴", "티아민", "리보플라빈", "니아신", "비타민C", "비타민D", "콜레스테롤", "포화지방", "트랜스지방"],
+			//섭취 음식 성분 이름
 		datasets: [
 			{
 				label: "필요영양소",
@@ -64,9 +65,9 @@ function barChart(grapeData) {
 	
 
 
-function lineChart(grapeData) {
+function lineChart(grapeData) {	// 선그래프
 	
-	const nutrients = [
+	const nutrients = [	//DB에서 받아오는 성분과 비교하여 출력하기 위한 배열
 		            'kcal', 'water', 'protein', 'fat', 'carbohydrate', 'sugars',
 		            'dietaryFiber', 'calcium', 'ironContent', 'phosphorus', 
 		            'potassium', 'sodium', 'vitaminA', 'retinol', 'betaCarotene',
@@ -74,7 +75,7 @@ function lineChart(grapeData) {
 		            'cholesterol', 'saturatedFat', 'transFat'
 	];
 	
-	const nutrientsKorean = [
+	const nutrientsKorean = [	//그래프 라벨 한글화
 			            'kcal', '물', '단백질', '지방', '탄수화물', '당류', '식이섬유', '칼슘', 
 						'철분함량', '인', '칼륨', '나트륨', '비타민A', '레티놀', '베타카로틴', '티아민', 
 						'리보플라빈', '니아신', '비타민C', '비타민D', '콜레스테롤', '포화지방', '트랜스지방'
@@ -84,15 +85,15 @@ function lineChart(grapeData) {
 	
 	let ctx = [];
 	
-	for (let i = 0; i < nutrients.length; i++) {
+	for (let i = 0; i < nutrients.length; i++) {	//선그래프 성분별로 가짓수 많큼 출력
 		
 		ctx.push(document.getElementsByClassName("lineCanvas")[i].getContext("2d"));
 
-		let XLabel = [];
+		let XLabel = [];	//날짜 출력 배열
 		
-		let nutrienStandard = [];
+		let nutrienStandard = [];	//성분별 기준치 출력 배열
 		
-		let nutrientData = [];
+		let nutrientData = [];	//성분별 추이 데이터 출력
 		
 		for (let j = 0; j < grapeData[1].length; j++) {
 		
